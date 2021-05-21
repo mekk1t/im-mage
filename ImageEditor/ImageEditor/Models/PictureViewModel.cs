f117f12dd5cad9a0bb7be3bd0b6ce8c4c52d7dff
mekk1t <kit.kalimov@gmail.com>
2021-05-21 23:07:31 +0300

    Удалил бутстрап и прочее

---
+++ ImageEditor/ImageEditor/Models/PictureViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageEditor.Models
+{
+    public class PictureViewModel
+    {
+        public string Base64 { get; set; }
+        public string ContentType { get; set; }
+    }
+}
