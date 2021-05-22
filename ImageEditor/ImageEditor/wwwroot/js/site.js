9d931760c8e1d71d4121254056947f12cac15faf
mekk1t <61093487+mekk1t@users.noreply.github.com>
2021-05-22 13:30:20 +0300

    Базовая верстка интерфейса (#2)
    * Revert "Базовый проект веб-приложения и основные библиотеки (#1)"
    
    This reverts commit 8e42ef7f8eb1c0ca0e973c0e4f0b77b3c0f43096.
    
    * Шаблонный проект MVC
    
    * Удалил бутстрап и прочее
    
    * Create w3.css
    
    * Update _Layout.cshtml
    
    * Update _Layout.cshtml
    
    * Начало верстки интерфейса
    
    * Кастомные классы утилиты
    
    * Update layout.css
    
    * Верстка базового интерфейса
    
    * фоновая картинка
    
    * вфывфы
    
    * Наверстал выбор расширений файла для сохранения
    
    * Верстка управления масштабом изображения
    
    * Улучшенная верстка управления масштабом изображения
    
    * Update _Layout.cshtml
    
    * Функция и класс для выделения выбранной кнопки
    
    * Попытка выделить границу через кастомные классы
    
    * Упрощение выделения нажатой/выбранной кнопки
    
    * Вывод других фильтров
    
    * override подсветки кнопок
    
    * Update _Layout.cshtml
    
    * Рендер всех фильтров
    
    * Управление яркостью и контрастностью
    
    * Верстка управления поворотами
    
    * Последний штрих

---
+++ ImageEditor/ImageEditor/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿function toggleElement(id) {
+    let element = document.getElementById(id);
+    element.classList.toggle("w3-hide");
+}
+
+function calculateInput(containerId) {
+    let container = document.getElementById(containerId);
+    let currentSize = container.querySelector('input');
+    let sizeOutput = container.querySelector('output');
+    sizeOutput.value = `${currentSize.value}%`;
+}
+function toggleButtonSelectionStatus(button) {
+    button.classList.toggle("selected");
+}
+
+function toggleFilters() {
+    let toggleButton = document.getElementById("toggle-more-filters-button");
+    let moreFiltersDiv = document.getElementById("more-filters");
+    if (moreFiltersDiv.classList.contains("w3-hide")) {
+        toggleButton.textContent = "Спрятать";
+    } else {
+        toggleButton.textContent = "Показать другие";
+    }
+
+    moreFiltersDiv.classList.toggle("w3-hide");
+}
