e2ba8cf7ee7f3b9c2f2a1419e70bb8592c16047e
mekk1t <kit.kalimov@gmail.com>
2021-05-22 11:13:15 +0300

    Улучшенная верстка управления масштабом изображения

--- ImageEditor/ImageEditor/wwwroot/js/site.js
+++ ImageEditor/ImageEditor/wwwroot/js/site.js
@@ -2,3 +2,9 @@
     let element = document.getElementById(id);
     element.classList.toggle("w3-hide");
 }
+
+function calculateImageSize() {
+    let currentSize = document.getElementById("zoom-input");
+    let sizeOutput = document.getElementById("image-size");
+    sizeOutput.value = `${currentSize.value}%`;
+}
