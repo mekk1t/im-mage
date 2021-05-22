1181a6dff2b668c80a93255c4007cc9250782c90
mekk1t <kit.kalimov@gmail.com>
2021-05-22 11:02:12 +0300

    Верстка управления масштабом изображения

--- ImageEditor/ImageEditor/wwwroot/js/site.js
+++ ImageEditor/ImageEditor/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function toggleElement(id) {
+    let element = document.getElementById(id);
+    element.classList.toggle("w3-hide");
+}
