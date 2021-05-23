8d61bf9f5b9c766177783e061ecf8bd605ec38b3
mekk1t <kit.kalimov@gmail.com>
2021-05-23 13:47:44 +0300

    Реализация смены яркости/контрастности относительно имеющегося результата

--- ImageEditor/ImageEditor/wwwroot/js/site.js
+++ ImageEditor/ImageEditor/wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿function toggleElement(id) {
+﻿var ORIGINAL_BASE_64 = "";
+var CONTENT_TYPE = "";
+var BRIGHTNESS_LEVEL = 0;
+var CONTRAST_LEVEL = 0;
+
+
+function toggleElement(id) {
     let element = document.getElementById(id);
     element.classList.toggle("w3-hide");
 }
@@ -50,10 +56,10 @@ function selectImage() {
             success: function (result) {
                 $("div.editing-image").html(result);
                 document.getElementById('upload-image-modal').style.display = 'none';
-                let originalBase64 = document.getElementById('image-base64-hidden').value;
-                $("#image-original").val(originalBase64);
-                let contentType = document.getElementById('image-contentType-hidden').value;
-                $("#content-type-original").val(contentType);
+                ORIGINAL_BASE_64 = document.getElementById('image-base64-hidden').value;
+                CONTENT_TYPE = document.getElementById('image-contentType-hidden').value;
+                BRIGHTNESS_LEVEL = 100;
+                CONTRAST_LEVEL = 100;
             },
             error: function (xhr) {
                 alert(xhr.responseText);
@@ -65,7 +71,7 @@ function selectImage() {
 function getImageObject() {
     let result = {
         Base64: document.getElementById('image-base64-hidden').value,
-        ContentType: document.getElementById('image-contentType-hidden').value
+        ContentType: CONTENT_TYPE
     };
     return result;
 }
@@ -109,17 +115,19 @@ function flipImage(flip) {
 }
 
 function changeBrightness() {
-    let image = getImageObject();
+    let brightness = $("#brightness input").val();
     $.ajax({
         url: "/api/images/transformations/brightness",
         type: 'POST',
         data: JSON.stringify({
-            Image: image,
-            BrightnessLevel: $("#brightness input").val()
+            Image: { Base64: ORIGINAL_BASE_64, ContentType: CONTENT_TYPE  },
+            BrightnessLevel: brightness
         }),
         contentType: 'application/json',
         success: function (result) {
             $("div.editing-image").html(result);
+            BRIGHTNESS_LEVEL = brightness;
+            allowBrightnessOrNot();
         },
         error: function (xhr) {
             alert(xhr.responseText);
@@ -128,17 +136,19 @@ function changeBrightness() {
 }
 
 function changeContrast() {
-    let image = getImageObject();
+    let contrast = $("#contrast input").val();
     $.ajax({
         url: "/api/images/transformations/contrast",
         type: 'POST',
         data: JSON.stringify({
-            Image: image,
-            ContrastLevel: $("#contrast input").val()
+            Image: { Base64: ORIGINAL_BASE_64, ContentType: CONTENT_TYPE },
+            ContrastLevel: contrast
         }),
         contentType: 'application/json',
         success: function (result) {
             $("div.editing-image").html(result);
+            CONTRAST_LEVEL = Number.parseInt(contrast);
+            allowContrastOrNot();
         },
         error: function (xhr) {
             alert(xhr.responseText);
@@ -146,10 +156,32 @@ function changeContrast() {
     });
 }
 
+function allowContrastOrNot() {
+    if (ORIGINAL_BASE_64 != "") {
+        let contrast = $("#contrast input").val();
+        let button = document.querySelector("#contrast button");
+        if (Number.parseInt(contrast) == CONTRAST_LEVEL) {
+            button.style.display = 'none';
+        } else {
+            button.style.display = 'block';
+        }
+    }
+}
+
+function allowBrightnessOrNot() {
+    if (ORIGINAL_BASE_64 != "") {
+        let brightness = $("#brightness input").val();
+        let button = document.querySelector("#brightness button");
+        if (Number.parseInt(brightness) == BRIGHTNESS_LEVEL) {
+            button.style.display = 'none';
+        } else {
+            button.style.display = 'block';
+        }
+    }
+}
+
 function reset() {
-    let contentType = $("#content-type-original").val();
-    let imageBase64 = $("#image-original").val();
     let img = document.querySelector("img");
-    img.src = `data:${contentType};base64, ${imageBase64}`;
-    $("#image-base64-hidden").val(imageBase64);
+    img.src = `data:${CONTENT_TYPE};base64, ${ORIGINAL_BASE_64}`;
+    $("#image-base64-hidden").val(ORIGINAL_BASE_64);
 }
